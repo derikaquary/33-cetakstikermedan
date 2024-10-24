5d140e40c8029fc6e5854ba11d8402fb5416851e
derikaquary <162952502+derikaquary@users.noreply.github.com>
2024-10-24 13:22:02 +0800

    added google tag manager

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,10 @@
+// layout.js
 import { Poppins } from "next/font/google";
 import "./_style/globals.css";
 import Header from "@/app/_components/Header";
 import WhatsApp from "./_components/WhatsApp";
 import UpArrow from "./_components/UpArrow";
+import Script from "next/script"; // Import Next.js Script component
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -17,11 +19,36 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-KB57L67B');
+            `,
+          }}
+        />
+      </Head>
       <body className={`${poppins.className}`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KB57L67B"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <Header />
         {children}
         <WhatsApp />
-        <UpArrow/>
+        <UpArrow />
       </body>
     </html>
   );
